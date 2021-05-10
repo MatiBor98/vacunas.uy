6941971d98a01ccec7f1cb739de7964272eb0bc6
Nicolás San Martín <nicolassanmar@gmail.com>
2021-05-10 15:33:33 -0300

    ponele

---
+++ sist-central/sist-central-ejb/src/main/java/datos/repositorios/VacunatorioRepository.java
@@ -0,0 +1,46 @@
+package datos.repositorios;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Singleton;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import datos.entidades.Vacunatorio;
+import datos.entidades.PlanVacunacion;
+
+/**
+ * Session Bean implementation class VacunatorioRepository
+ */
+@Singleton
+@LocalBean
+public class VacunatorioRepository implements VacunatorioRepositoryLocal {
+
+	@Inject
+    private EntityManager entityManager;
+
+    public VacunatorioRepository() {
+    }
+
+    public List<Vacunatorio> find() {
+        return entityManager.createQuery("select e from Vacunatorio e", Vacunatorio.class)
+                .getResultList();
+    }
+
+    public void save(Vacunatorio vac) {
+        entityManager.persist(vac);
+    }
+
+    public Optional<Vacunatorio> find(String nombre) {
+        List<Vacunatorio> resultList = entityManager.createQuery(
+                "select e from Vacunatorio e where e.nombre = :nombre",
+                Vacunatorio.class)
+                .setParameter("nombre", nombre)
+                .setMaxResults(1)
+                .getResultList();
+        return resultList.isEmpty() ? Optional.empty() : Optional.of(resultList.get(0));
+    }
+}
+
---
+++ sist-central/sist-central-ejb/src/main/java/datos/repositorios/VacunatorioRepositoryLocal.java
@@ -0,0 +1,15 @@
+package datos.repositorios;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.ejb.Local;
+
+import datos.entidades.Vacunatorio;
+
+@Local
+public interface VacunatorioRepositoryLocal {
+	 public List<Vacunatorio> find();
+	 public void save(Vacunatorio vac);
+	 public Optional<Vacunatorio> find(String nombre);
+}
