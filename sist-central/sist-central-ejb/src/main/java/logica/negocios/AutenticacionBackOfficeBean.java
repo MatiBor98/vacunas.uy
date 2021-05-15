6f9ffb5adb6b9ee4907705c6d353a1e64900b492
agustin.ruiz.diaz <agustin.ruiz.diaz@fing.edu.uy>
2021-05-15 15:43:48 -0300

    capa de negocio ready

--- sist-central/sist-central-ejb/src/main/java/logica/negocios/AutenticacionBackOfficeBean.java
+++ sist-central/sist-central-ejb/src/main/java/logica/negocios/AutenticacionBackOfficeBean.java
@@ -1,9 +1,12 @@
 package logica.negocios;
 
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
+import datos.entidades.UsuarioBO;
 import datos.exceptions.EmailNoRegistradoException;
 import datos.exceptions.PasswordIncorrectaException;
+import datos.repositorios.UsuariosBackOfficeRepositoryLocal;
 import logica.servicios.local.AutenticacionBackOfficeBeanLocal;
 
 /**
@@ -12,6 +15,9 @@ import logica.servicios.local.AutenticacionBackOfficeBeanLocal;
 @Stateless
 public class AutenticacionBackOfficeBean implements AutenticacionBackOfficeBeanLocal {
 
+	@EJB
+	UsuariosBackOfficeRepositoryLocal usuariosBO;
+	
     /**
      * Default constructor. 
      */	
@@ -19,7 +25,16 @@ public class AutenticacionBackOfficeBean implements AutenticacionBackOfficeBeanL
         // TODO Auto-generated constructor stub
     }
 
-    public auntenticarUsuario(String email, String password) throws EmailNoRegistradoException, PasswordIncorrectaException{
+    public UsuarioBO auntenticarUsuario(String email, String password) throws EmailNoRegistradoException, PasswordIncorrectaException{
     	
+    	UsuarioBO usuario = usuariosBO.find(email);
+    	if(usuario == null) {
+    		throw new EmailNoRegistradoException();
+    	}
+    	if(!usuario.getPassword().equals(password)){
+    		throw new PasswordIncorrectaException();
+    	}
+    	return usuario;
     }
+    
 }
