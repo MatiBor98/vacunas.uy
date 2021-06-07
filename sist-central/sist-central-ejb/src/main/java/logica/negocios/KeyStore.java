27a7fdabf45293426d8a1d1384d0530f64478801
agustin.ruiz.diaz <agustin.ruiz.diaz@fing.edu.uy>
2021-06-07 15:36:20 -0300

    no compilaba

--- sist-central/sist-central-ejb/src/main/java/logica/negocios/KeyStore.java
+++ sist-central/sist-central-ejb/src/main/java/logica/negocios/KeyStore.java
@@ -1,9 +1,8 @@
 package logica.negocios;
 
-import java.nio.charset.StandardCharsets;
-
 import javax.annotation.PostConstruct;
 import javax.crypto.SecretKey;
+import javax.ejb.LocalBean;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 
@@ -15,9 +14,13 @@ import logica.servicios.local.KeyStoreLocal;
  * Session Bean implementation class KeyStore
  */
 @Singleton
+@LocalBean
 @Startup
 public class KeyStore implements KeyStoreLocal {
 
+    /**
+     * Default constructor. 
+     */
 	private SecretKey key;
 	
     /**
@@ -38,5 +41,4 @@ public class KeyStore implements KeyStoreLocal {
 		return key;
 	}
 
-    
 }
