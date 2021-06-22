3df2e361c046ecc2d9bdf4d484bb0aa33b6762bb
User <User@DESKTOP-E2M605F>
2021-06-21 21:34:25 -0300

    vacunatorio funcionando piola

--- Vacunatorio/Vacunatorio-ejb/src/main/java/logica/inicio/StartupBean.java
+++ Vacunatorio/Vacunatorio-ejb/src/main/java/logica/inicio/StartupBean.java
@@ -1,5 +1,6 @@
 package logica.inicio;
 
+import java.security.GeneralSecurityException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -26,11 +27,27 @@ import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
+import org.apache.http.ssl.SSLContexts;
 import org.eclipse.microprofile.config.Config;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import javax.net.ssl.SSLContext;
 
 import datos.dtos.AgendaDTO2;
 import datos.dtos.AsignacionDTO;
@@ -117,146 +134,154 @@ public class StartupBean {
 	}
 	
 	public void run() {
-    	Client client = ClientBuilder.newClient();
+		
+		
+    	/*Client client = ClientBuilder.newClient();
 		WebTarget target = client.target("http://vacunas.web.elasticloud.uy/rest/vacunatorios/vacunatorio/"+config.getValue("nombre", String.class)); 
 		Invocation invocation = target.request().buildGet();
-		Response response = invocation.invoke();		
-		datos = response.readEntity(DatosVacunatorio.class);
+		Response response = invocation.invoke();
+		datos = response.readEntity(DatosVacunatorio.class);*/
 
-		Properties props = new Properties();
-		props.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
-		props.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
-		props.put(Context.PROVIDER_URL, "http-remoting://vacunas.web.elasticloud.uy/rest/vacunatorios/vacunatorio/"+config.getValue("nombre", String.class));
-		reservaRepository.drop();
-		intervaloRepository.drop();
-		agendaRepository.drop();
-		etapaRepository.drop();
-		planVacunacionRepository.drop();
-		pVacRepository.dropAsignaciones();
-		pVacRepository.drop();
-		turnoRepository.drop();
-		ciudadanoRepository.drop();
-		vacRepository.drop();
-		loteRepository.drop();
-		vacunaRepository.drop();
-
-		
-		VacunatorioDTO vacDTO = datos.getVac();
-		Vacunatorio vacunatorio = new Vacunatorio(vacDTO.getNombre(), vacDTO.getCiudad(), vacDTO.getDireccion(), vacDTO.getDepartamento());
-		vacRepository.save(vacunatorio);
-		List<Vacunatorio> vacs = vacRepository.find();
-		Vacunatorio vac = vacs.get(0);
-		
-		//asignaciones
-		
-		for(TurnoDTO turnoDTO:vacDTO.getTurnos()) {
-			Turno turno = new Turno(turnoDTO.getNombre(), LocalTime.parse(turnoDTO.getInicio()), LocalTime.parse(turnoDTO.getFin()), vac);
-			turnoRepository.save(turno);
-		}
-		List<PuestoVacunacion> pVacs = new ArrayList<>();
-		List<Vacunador> vacunadores = new ArrayList<>();
-		for(PuestoVacunacionDTO pVacDTO:vacDTO.getPuestosVacunacion()) {
-			PuestoVacunacion pVac = new PuestoVacunacion(vac, pVacDTO.getNombrePuesto());
-			pVacRepository.save(pVac);
-			List<Asignacion> asigs =  new ArrayList<>();
-			for(AsignacionDTO asigDTO:pVacDTO.getAsignaciones()) {
-				Turno turnoAsignado = turnoRepository.find(vac.getNombre(), asigDTO.getTurno().getNombre()).get(0);
-				Vacunador vacunador = null;
-				if(!existeVacunador(asigDTO.getVacunador().getCi(), vacunadores)) {				
-					vacunador = new Vacunador(asigDTO.getVacunador().getCi(), asigDTO.getVacunador().getEmail(), asigDTO.getVacunador().getNombre());
+		try {
+			datos = enviarPorHTTPS();
+			
+			/*Properties props = new Properties();
+			props.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
+			props.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+			props.put(Context.PROVIDER_URL, "https-remoting://node1531-vacunas.web.elasticloud.uy/rest/vacunatorios/vacunatorio/"+config.getValue("nombre", String.class));*/
+			reservaRepository.drop();
+			intervaloRepository.drop();
+			agendaRepository.drop();
+			etapaRepository.drop();
+			planVacunacionRepository.drop();
+			pVacRepository.dropAsignaciones();
+			pVacRepository.drop();
+			turnoRepository.drop();
+			ciudadanoRepository.drop();
+			vacRepository.drop();
+			loteRepository.drop();
+			vacunaRepository.drop();
+			
+			
+			VacunatorioDTO vacDTO = datos.getVac();
+			Vacunatorio vacunatorio = new Vacunatorio(vacDTO.getNombre(), vacDTO.getCiudad(), vacDTO.getDireccion(), vacDTO.getDepartamento());
+			vacRepository.save(vacunatorio);
+			List<Vacunatorio> vacs = vacRepository.find();
+			Vacunatorio vac = vacs.get(0);
+			
+			//asignaciones
+			
+			for(TurnoDTO turnoDTO:vacDTO.getTurnos()) {
+				Turno turno = new Turno(turnoDTO.getNombre(), LocalTime.parse(turnoDTO.getInicio()), LocalTime.parse(turnoDTO.getFin()), vac);
+				turnoRepository.save(turno);
+			}
+			List<PuestoVacunacion> pVacs = new ArrayList<>();
+			List<Vacunador> vacunadores = new ArrayList<>();
+			for(PuestoVacunacionDTO pVacDTO:vacDTO.getPuestosVacunacion()) {
+				PuestoVacunacion pVac = new PuestoVacunacion(vac, pVacDTO.getNombrePuesto());
+				pVacRepository.save(pVac);
+				List<Asignacion> asigs =  new ArrayList<>();
+				for(AsignacionDTO asigDTO:pVacDTO.getAsignaciones()) {
+					Turno turnoAsignado = turnoRepository.find(vac.getNombre(), asigDTO.getTurno().getNombre()).get(0);
+					Vacunador vacunador = null;
+					if(!existeVacunador(asigDTO.getVacunador().getCi(), vacunadores)) {				
+						vacunador = new Vacunador(asigDTO.getVacunador().getCi(), asigDTO.getVacunador().getEmail(), asigDTO.getVacunador().getNombre());
+						try {
+							ciudadanoRepository.save(vacunador);
+						} catch (CiudadanoRegistradoException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						vacunadores.add(vacunador);
+					} 
+					Vacunador vacAsig = (Vacunador) ciudadanoRepository.findByNombreCi(asigDTO.getVacunador().getCi());
+					Turno turnoAsig = turnoRepository.find(vac.getNombre(), asigDTO.getTurno().getNombre()).get(0);
+					PuestoVacunacion pVacAsig = pVacRepository.find(pVacDTO.getNomVacunatorio(), pVacDTO.getNombrePuesto()).get(0);
+					DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy");
+					Asignacion asig = new Asignacion(vacAsig, turnoAsig, pVacAsig, LocalDate.parse(asigDTO.getFechaInicio(), formatter), LocalDate.parse(asigDTO.getFechaFin(), formatter));
+					pVacRepository.addAsignacion(asig);
+				}
+			}
+			
+			//reservas
+			
+			List<Reserva> reservas = new ArrayList<>();
+			for(ReservaDTO reservaDTO:datos.getReservas()) {
+				Reserva res = new Reserva();
+				res.setCodigo(reservaDTO.getCodigo());
+				res.setParaDosis(reservaDTO.getParaDosis());
+				res.setEstado(reservaDTO.getEstado());
+				res.setLote(reservaDTO.getLote());
+				if(ciudadanoRepository.findByNombreCi(reservaDTO.getCiudadano().getCi()) == null) {
+					Ciudadano ciud = new Ciudadano(reservaDTO.getCiudadano().getCi(), reservaDTO.getCiudadano().getNombre(), reservaDTO.getCiudadano().getEmail());
 					try {
-						ciudadanoRepository.save(vacunador);
+						ciudadanoRepository.save(ciud);
 					} catch (CiudadanoRegistradoException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-					vacunadores.add(vacunador);
-				} 
-				Vacunador vacAsig = (Vacunador) ciudadanoRepository.findByNombreCi(asigDTO.getVacunador().getCi());
-				Turno turnoAsig = turnoRepository.find(vac.getNombre(), asigDTO.getTurno().getNombre()).get(0);
-				PuestoVacunacion pVacAsig = pVacRepository.find(pVacDTO.getNomVacunatorio(), pVacDTO.getNombrePuesto()).get(0);
-				DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy");
-				Asignacion asig = new Asignacion(vacAsig, turnoAsig, pVacAsig, LocalDate.parse(asigDTO.getFechaInicio(), formatter), LocalDate.parse(asigDTO.getFechaFin(), formatter));
-				pVacRepository.addAsignacion(asig);
-			}
-		}
-		
-		//reservas
-		
-		List<Reserva> reservas = new ArrayList<>();
-		for(ReservaDTO reservaDTO:datos.getReservas()) {
-			Reserva res = new Reserva();
-			res.setCodigo(reservaDTO.getCodigo());
-			res.setParaDosis(reservaDTO.getParaDosis());
-			res.setEstado(reservaDTO.getEstado());
-			res.setLote(reservaDTO.getLote());
-			if(ciudadanoRepository.findByNombreCi(reservaDTO.getCiudadano().getCi()) == null) {
-				Ciudadano ciud = new Ciudadano(reservaDTO.getCiudadano().getCi(), reservaDTO.getCiudadano().getNombre(), reservaDTO.getCiudadano().getEmail());
-				try {
-					ciudadanoRepository.save(ciud);
-				} catch (CiudadanoRegistradoException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
 				}
-			}
-			Ciudadano ciudadano = ciudadanoRepository.findByNombreCi(reservaDTO.getCiudadano().getCi());
-			res.setCiudadano(ciudadano);
-			PlanVacunacionDTO2 planVacDTO = reservaDTO.getIntervalo().getAgenda2().getEtapa().getPlanVac();
-			if(planVacunacionRepository.findByNombre(planVacDTO.getNombre()) == null) {
-				PlanVacunacion planVac = new PlanVacunacion(planVacDTO.getNombre(), LocalDate.parse(planVacDTO.getInicio()), LocalDate.parse(planVacDTO.getFin()), planVacDTO.getEnfermedad());
-				planVacunacionRepository.save(planVac);
-			}
-			PlanVacunacion planVacunacion = planVacunacionRepository.findByNombre(planVacDTO.getNombre());
-			EtapaDTO2 etapaDTO = reservaDTO.getIntervalo().getAgenda2().getEtapa();
-			if(etapaRepository.find(etapaDTO) == null) {
-				Etapa etap = new Etapa(etapaDTO.getDescripcion(), LocalDate.parse(etapaDTO.getInicio()), LocalDate.parse(etapaDTO.getFin()), etapaDTO.getVacuna(), planVacunacion);
-				etapaRepository.save(etap);
-			}
-			Etapa etapa = etapaRepository.find(etapaDTO);
-			AgendaDTO2 agendaDTO = reservaDTO.getIntervalo().getAgenda2();
-			Turno turno = turnoRepository.find(vac.getNombre(), agendaDTO.getTurno().getNombre()).get(0);
-			if(agendaRepository.find(agendaDTO.getNombre(), etapa, agendaDTO.getTurno().getNombre()) == null) {
-				//en caso de que no se haya definido una fecha de fin
-				LocalDate fechaFin;
-				if(agendaDTO.getFin().equals("")) {
-					fechaFin = null;
-				} else {
-					fechaFin = LocalDate.parse(agendaDTO.getFin());
+				Ciudadano ciudadano = ciudadanoRepository.findByNombreCi(reservaDTO.getCiudadano().getCi());
+				res.setCiudadano(ciudadano);
+				PlanVacunacionDTO2 planVacDTO = reservaDTO.getIntervalo().getAgenda2().getEtapa().getPlanVac();
+				if(planVacunacionRepository.findByNombre(planVacDTO.getNombre()) == null) {
+					PlanVacunacion planVac = new PlanVacunacion(planVacDTO.getNombre(), LocalDate.parse(planVacDTO.getInicio()), LocalDate.parse(planVacDTO.getFin()), planVacDTO.getEnfermedad());
+					planVacunacionRepository.save(planVac);
 				}
-				Agenda agend = new Agenda(agendaDTO.getNombre(), LocalDate.parse(agendaDTO.getInicio()), fechaFin, etapa, turno);
-				agendaRepository.save(agend);
-			}
-			Agenda agenda = agendaRepository.find(agendaDTO.getNombre(), etapa, agendaDTO.getTurno().getNombre());
-			if(intervaloRepository.findByFecha(agenda.getId(), LocalDateTime.parse(reservaDTO.getIntervalo().getFechayHora())).isEmpty()) {
-				Intervalo inter = new Intervalo(LocalDateTime.parse(reservaDTO.getIntervalo().getFechayHora()), agenda);
-				intervaloRepository.save(inter);
-			}
-			Intervalo intervalo = null;
-			List<Intervalo> intervalos = intervaloRepository.find();				
-			for (Intervalo interv:intervalos) {
-				if (interv.getFechayHora().isEqual(LocalDateTime.parse(reservaDTO.getIntervalo().getFechayHora()))) {
-					intervalo = interv;
-					break;
+				PlanVacunacion planVacunacion = planVacunacionRepository.findByNombre(planVacDTO.getNombre());
+				EtapaDTO2 etapaDTO = reservaDTO.getIntervalo().getAgenda2().getEtapa();
+				if(etapaRepository.find(etapaDTO) == null) {
+					Etapa etap = new Etapa(etapaDTO.getDescripcion(), LocalDate.parse(etapaDTO.getInicio()), LocalDate.parse(etapaDTO.getFin()), etapaDTO.getVacuna(), planVacunacion);
+					etapaRepository.save(etap);
 				}
+				Etapa etapa = etapaRepository.find(etapaDTO);
+				AgendaDTO2 agendaDTO = reservaDTO.getIntervalo().getAgenda2();
+				Turno turno = turnoRepository.find(vac.getNombre(), agendaDTO.getTurno().getNombre()).get(0);
+				if(agendaRepository.find(agendaDTO.getNombre(), etapa, agendaDTO.getTurno().getNombre()) == null) {
+					//en caso de que no se haya definido una fecha de fin
+					LocalDate fechaFin;
+					if(agendaDTO.getFin().equals("")) {
+						fechaFin = null;
+					} else {
+						fechaFin = LocalDate.parse(agendaDTO.getFin());
+					}
+					Agenda agend = new Agenda(agendaDTO.getNombre(), LocalDate.parse(agendaDTO.getInicio()), fechaFin, etapa, turno);
+					agendaRepository.save(agend);
+				}
+				Agenda agenda = agendaRepository.find(agendaDTO.getNombre(), etapa, agendaDTO.getTurno().getNombre());
+				if(intervaloRepository.findByFecha(agenda.getId(), LocalDateTime.parse(reservaDTO.getIntervalo().getFechayHora())).isEmpty()) {
+					Intervalo inter = new Intervalo(LocalDateTime.parse(reservaDTO.getIntervalo().getFechayHora()), agenda);
+					intervaloRepository.save(inter);
+				}
+				Intervalo intervalo = null;
+				List<Intervalo> intervalos = intervaloRepository.find();				
+				for (Intervalo interv:intervalos) {
+					if (interv.getFechayHora().isEqual(LocalDateTime.parse(reservaDTO.getIntervalo().getFechayHora()))) {
+						intervalo = interv;
+						break;
+					}
+				}
+				res.setIntervalo(intervalo);
+				reservaRepository.save(res);
+				
 			}
-			res.setIntervalo(intervalo);
-			reservaRepository.save(res);
+			//lotes
 			
-		}
-
-		//lotes
-		
-		Set<LoteDTO> lotesDTO = vacDTO.getLotes();
-		for(LoteDTO loteDTO:lotesDTO) {
-			if(vacunaRepository.findByNombreVacuna(loteDTO.getVacuna().getNombre()).isEmpty()) {
-				vacunaRepository.save(loteDTO.getVacuna().getNombre(), loteDTO.getVacuna().getCantDosis(), loteDTO.getVacuna().getInmunidadMeses(), loteDTO.getVacuna().getDosisSeparacionDias(), null, null);
+			Set<LoteDTO> lotesDTO = vacDTO.getLotes();
+			for(LoteDTO loteDTO:lotesDTO) {
+				if(vacunaRepository.findByNombreVacuna(loteDTO.getVacuna().getNombre()).isEmpty()) {
+					vacunaRepository.save(loteDTO.getVacuna().getNombre(), loteDTO.getVacuna().getCantDosis(), loteDTO.getVacuna().getInmunidadMeses(), loteDTO.getVacuna().getDosisSeparacionDias(), null, null);
+				}
+				Vacuna vacuna = vacunaRepository.findByNombreVacuna(loteDTO.getVacuna().getNombre()).get(0);
+				Lote lote = new Lote(loteDTO.getDosisDisponibles(), loteDTO.getNumeroLote(), LocalDate.parse(loteDTO.getFechaVencimiento()), vacuna, LocalDate.parse(loteDTO.getFechaEntrega()), LocalDate.parse(loteDTO.getFechaDespacho()));
+				loteRepository.save(lote);
 			}
-			Vacuna vacuna = vacunaRepository.findByNombreVacuna(loteDTO.getVacuna().getNombre()).get(0);
-			Lote lote = new Lote(loteDTO.getDosisDisponibles(), loteDTO.getNumeroLote(), LocalDate.parse(loteDTO.getFechaVencimiento()), vacuna, LocalDate.parse(loteDTO.getFechaEntrega()), LocalDate.parse(loteDTO.getFechaDespacho()));
-			loteRepository.save(lote);
+		} catch (GeneralSecurityException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
-    	
-    }
+	}
+
     Boolean existeVacunador(int ci, List<Vacunador> vacs) {
     	Boolean res = false;
     	for(Vacunador vac:vacs) {
@@ -339,5 +364,22 @@ public class StartupBean {
 			e.printStackTrace();
 		}
     }
+    
+    public DatosVacunatorio enviarPorHTTPS() throws GeneralSecurityException {
+		String urlOverHttps = "https://node1531-vacunas.web.elasticloud.uy/rest/vacunatorios/vacunatorio/"+config.getValue("nombre", String.class);
+		TrustStrategy acceptingTrustStrategy = (cert, authType) -> true;
+		SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
+		SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
+		Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+				.register("https", sslsf).register("http", new PlainConnectionSocketFactory()).build();
+		BasicHttpClientConnectionManager connectionManager = new BasicHttpClientConnectionManager(
+				socketFactoryRegistry);
+		CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(sslsf)
+				.setConnectionManager(connectionManager).build();
+		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+		ResponseEntity<DatosVacunatorio> response = new RestTemplate(requestFactory).exchange(urlOverHttps, HttpMethod.GET, null,
+				DatosVacunatorio.class);
+		return response.getBody();
+	}
 	
 }
