7f456db6c675dc00ca1b7e57c3448d4b0e9410ca
Nicolás San Martín <nicolassanmar@gmail.com>
2021-07-08 16:09:17 -0300

    esto puede buguearse si no lo cambiaba

--- sist-central/sist-central-ejb/src/main/java/datos/repositorios/ReservaRepository.java
+++ sist-central/sist-central-ejb/src/main/java/datos/repositorios/ReservaRepository.java
@@ -218,7 +218,7 @@ public class ReservaRepository implements ReservaRepositoryLocal{
                 + "join Etapa e on (a.etapa.id = e.id) "
                 + "join Vacuna v on (e.vacuna.nombre = v.nombre) "
                 + "join v.enfermedades enf "
-                + "where r.estado = 0 ";
+                + "where r.estado = :pendiente ";
         if (!enfermedad.equals(""))
             qlString += " and enf.nombre = :enfermedad ";
         if (!vacuna.equals(""))
@@ -228,7 +228,7 @@ public class ReservaRepository implements ReservaRepositoryLocal{
 
         Query query = entityManager.createQuery(
                 qlString)
-                //.setParameter("vacunado", Estado.PENDIENTE)
+                .setParameter("pendiente", Estado.PENDIENTE)
                 ;
 
         if (!enfermedad.equals(""))
